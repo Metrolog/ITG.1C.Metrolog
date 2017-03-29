a1ece22558d12428476aa77eec27c3c7a47eff66
Sergey S. Betke <sergey.s.betke@yandex.ru>
2017-03-30 00:26:54 +0300

    * заготовка сценария подготовки среды CI (#5)

---
+++ install/install.ps1
@@ -0,0 +1,104 @@
+﻿<# 
+.Synopsis 
+    Скрипт подготовки среды сборки и тестирования проекта
+.Description 
+    Скрипт подготовки среды сборки и тестирования проекта.
+    Используется в том числе и для подготовки среды на серверах appveyor.
+.Link 
+    https://github.com/Metrolog/MeasurementUnits
+.Example 
+    .\install.ps1 -GUI;
+    Устанавливаем необходимые пакеты, в том числе - и графические среды.
+#> 
+[CmdletBinding(
+    SupportsShouldProcess = $true
+    , ConfirmImpact = 'Medium'
+    , HelpUri = 'https://github.com/Metrolog/MeasurementUnits'
+)]
+ 
+param (
+    # Ключ, определяющий необходимость установки визуальных средств.
+    # По умолчанию устанавливаются только средства для командной строки.
+    [Switch]
+    $GUI
+) 
+
+Function Execute-ExternalInstaller {
+    [CmdletBinding(
+        SupportsShouldProcess = $true
+        , ConfirmImpact = 'Medium'
+    )]
+    param (
+        [String]
+        $LiteralPath
+        ,
+        [String]
+        $ArgumentList
+    )
+
+    $pinfo = [System.Diagnostics.ProcessStartInfo]::new();
+    $pinfo.FileName = $LiteralPath;
+    $pinfo.RedirectStandardError = $true;
+    $pinfo.RedirectStandardOutput = $true;
+    $pinfo.RedirectStandardInput = $true;
+    $pinfo.UseShellExecute = $false;
+    $pinfo.Arguments = $ArgumentList;
+    $p = [System.Diagnostics.Process]::new();
+    try {
+        $p.StartInfo = $pinfo;
+        $p.Start() | Out-Null;
+        $p.WaitForExit();
+        $LASTEXITCODE = $p.ExitCode;
+        $p.StandardOutput.ReadToEnd() `
+        | Write-Output `
+        ;
+        if ( $p.ExitCode -ne 0 ) {
+            $p.StandardError.ReadToEnd() `
+            | Write-Error `
+            ;
+        };
+    } finally {
+        $p.Close();
+    };
+}
+
+switch ( $env:PROCESSOR_ARCHITECTURE ) {
+    'amd64' { $ArchPath = 'x64'; }
+    'x86'   { $ArchPath = 'x86'; }
+    default { Write-Error 'Unsupported processor architecture.'}
+};
+$ToPath = @();
+
+if ( 
+    ( -not ( $env:APPVEYOR -eq 'True' ) ) `
+    -and ( -not $env:ChocolateyInstall ) `
+) {
+    Invoke-WebRequest -Uri 'https://chocolatey.org/install.ps1' -UseBasicParsing | Invoke-Expression;
+};
+
+& choco install GitVersion.Portable --confirm --failonstderr | Out-String -Stream | Write-Verbose;
+& choco install GitReleaseNotes.Portable --confirm --failonstderr | Out-String -Stream | Write-Verbose;
+
+if ( -not ( $env:APPVEYOR -eq 'True' ) ) {
+    & choco install git --confirm --failonstderr | Out-String -Stream | Write-Verbose;
+    & choco install openssl --confirm --failonstderr | Out-String -Stream | Write-Verbose;
+};
+
+if ( $GUI ) {
+    & choco install SourceTree --confirm --failonstderr | Out-String -Stream | Write-Verbose;
+};
+
+Write-Verbose 'Preparing PATH environment variable...';
+if ($PSCmdLet.ShouldProcess('PATH', 'Установить переменную окружения')) {
+    $Path = `
+        ( `
+            ( ( [Environment]::GetEnvironmentVariable( 'PATH', [System.EnvironmentVariableTarget]::Process ) ) -split ';' ) `
+            + $ToPath `
+            | Sort-Object -Unique `
+        ) `
+    ;
+    Write-Verbose 'Path variable:';
+    $Path | % { Write-Verbose "    $_" };
+    $env:Path = $Path -join ';';
+    [System.Environment]::SetEnvironmentVariable( 'PATH', $env:Path, [System.EnvironmentVariableTarget]::User );
+};
